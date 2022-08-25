2344e0efb0503d833713d55675c0eaced38e98b0
Aayush <106085120+AyushSharma64047@users.noreply.github.com>
2022-08-25 20:40:44 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,35 +4,35 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "PHListBot")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
-    ABOUT_BOT_TEXT = """<b>This is a TeleGram Search Bot of @Aks_support01_bot And Some Other Bots Available On TeleGram.
+    API_ID = int(os.environ.get("API_ID", "17954446" ))
+    API_HASH = os.environ.get("API_HASH", "b9d62d60af7542ad41274a47a5c9f369")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5470572629:AAHta93tGkvxqgDPaWJdXy1DfiZkGPN2ZHs")
+    BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "ZHARBOURMdisk")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQBKDXsN3vsQf1vTvnoKNrvYe8k-JZJwB-p_jSm23dRpQdpcozKo9h68AUmACvkcFMof6UJqsIFarjYn-rhdQce2D1A4y46jvLxphXJWLkaTsLizdgu_CSa0pucL0AL79GEzLj2WL3FtOe8I8lLJ2B-bVZIVHRVqC6QPguhoXmLNlXlTp3NoNEvHrD21iW4VR86_hTq5Avjh2zYJOxl2-pq8cbyD0w5DkRjKWzRxGDvQloWnlKjvLizX291zbw1m8VuemE6nL_PUfiKrsWKA8SRyN4ISpC4pZ74Bfi3Uzo9ysN3rIQuASGg4Bi_dWk_TofW6DJceUk6FoMFkikGjDm3WAAAAATp6iEcA")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001712731881" ))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME" , "MdiskLinksSenderBot")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER" ,"5276076103"))
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL","-1001557431626")
+    ABOUT_BOT_TEXT = """<b>This is a TeleGram Search Bot of @z_harbour And Some Other Bots Available On TeleGram.
 
 🤖 My Name: <a href='https://t.me/mdisk1_search_bot'> Mᴅɪsᴋ Sᴇᴀʀᴄʜ ʙᴏᴛ </a>
 
 📝 Language : <a href='https://www.python.org'> Python V3</a>
 
-📚 Library: <a href='https://docs.pyrogram.org'> Pyrogram </a>
+📚 Library: <a href='https://docs.pyrogram.org'> PythonPyrogram </a>
 
-📡 Server: <a href='https://heroku.com'> Heroku </a>
+📡 Server: <a href='https://heroku.com'> Heroku/Github </a>
 
-👨‍💻 Modified By: <a href='https://t.me/Z_Harbour_bot'>Imdb-updates</a>
+👨‍💻 Modified By: <a href='https://t.me/Z_Harbour_bot'> My Father</a>
 
-🌀 Youtube: <a href='https://youtube.com/channel/UCyo2YOr51okeUIpEyM7ZGkw'>ZHarbourYT</a>
+🌀 Youtube: <a href='https://youtube.com/channel/UCyo2YOr51okeUIpEyM7ZGkw'>AllInOneYT</a>
 
 👥 Bots Support: <a href='https://t.me/Z_Harbour_bot'>IMDBBOTS</a>
 
-📢 Bots Updates: <a href='https://t.me/Z_Harbour_bot'>IMDB-BOTS</a></b>
+👨‍💻 Father's Channel: <a href='https://t.me/z_harbour'>z_harbour</a></b>
 """
     
-    ABOUT_HELP_TEXT = """<b>👨‍💻 Developers : <a href='https://t.me/Z_Harbour_bot'>Lucifer</a>
+    ABOUT_HELP_TEXT = """<b>👨‍💻 Developers : <a href='https://t.me/Z_Harbour_bot'> Lucifer </a>
 
 Bots are simply Telegram accounts operated by software – not people – and they'll often have AI features. They can do anything – teach, play, search, broadcast, remind, connect, integrate with other services, or even pass commands to the Internet of Things.
 
@@ -54,12 +54,12 @@ Bots are simply Telegram accounts operated by software – not people – and t
 
        <a> Credits goes to Everyone Who Supported.</b>
 
-<a> Made With ❤ By @Aks_support01_bot </a>
+<a> Made With ❤ By @z_harbour </a>
 """
 
 
     START_MSG = """
-<b>👋 Hey {}, 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂  <a href='https://t.me/mdisk1_search_bot'>mdisksearchbot</a>.
-<a> 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽.. 𝚃𝙷𝙴𝙽 𝚂𝙴𝙴 𝙼𝚈 𝙿𝙾𝚆𝙴𝚁𝚂 ♥️♥️🔥 </a>
+<b>👋 Hey {}, 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂  <a href='https://t.me/MdiskLinksSenderBot'>mdisksearchbot</a>.
+<a> 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂-SEREIS, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽... 𝚃𝙷𝙴𝙽 𝚂𝙴𝙴 𝙼𝚈 𝙿𝙾𝚆𝙴𝚁𝚂🔥 </a>
 """
     ADD_BOTS = """<b>Heya! {} Want Same Like This Then Contact The Support.</b>"""
